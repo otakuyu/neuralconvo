c3b55ebd6de5c604946a4497368583aa566c59b1
Alex Yu <yuquanzhou_007@hotmail.com>
2017-06-04 21:11:27 +1000

    added tornado web server

---
+++ app.py
@@ -0,0 +1,20 @@
+import lutorpy as lua
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = tornado.escape.json_decode(self.request.body)
+        self.write(returnSay(data["message"]))
+
+    def get(self):
+        self.write("Brain!")
+
+if __name__ == "__main__":
+    returnSay = require("eval")
+    application = tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+    application.listen(8888)
+    print "Brain started listen on port 8888"
+    tornado.ioloop.IOLoop.current().start()
