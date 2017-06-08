a9320540d014e1d50afa958482c1cb081e824cd2
Alex Yu <yuquanzhou_007@hotmail.com>
2017-06-08 22:18:59 +1000

    added s3 bucket name as environment variable

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 import lutorpy as lua
 import boto3
+import os
+
+print os.environ['S3_BUCKET_NAME']
 
 train = require("trainer")
 
 train()
 
 s3 = boto3.resource('s3')
-s3.meta.client.upload_file('data/model.t7', 'chatbot-deploy-bucket', 'model.t7')
+s3.meta.client.upload_file('data/model.t7', os.environ['S3_BUCKET_NAME'], 'model.t7')
